ad3611a4893476d17684a00041618a980195eb3f
MTGini22 <mmangini27@gsb.columbia.edu>
2026-05-29 09:09:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const V='wt-v110';
+const FILES=['/Workouts/','/Workouts/index.html'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==V).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      var copy=res.clone();
+      caches.open(V).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
+});
